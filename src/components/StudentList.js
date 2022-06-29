3ba5cbd275441621b811c8bd5585e8ea2275e62e
panfrieddupa <contact@bekahthe.dev>
2022-06-29 17:03:44 -0600

    Adds dynamic funcationality and styling
    Maps StudentList as props for Students
    Creates and inputs dynamic data to Cards in Students from StudentList
    Requires importing of images dynamically by moving folders
    Styles the Students Cards into a grid and gives them some color

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,33 +1,39 @@
-import React from "react"
+import React from 'react'
+import Students from './Students';
 
-function StudentList () {
+function StudentList() {
 
     const cardInfo = [
         {
-          image: "s1.png",
-          name: "Mike",
-          gpa: 3.8
+            image: 's1.png',
+            name: 'Mike',
+            gpa: 3.8
         },
         {
-          image: "s2.png",
-            name: "Manal",
+            image: 's2.png',
+            name: 'Manal',
             gpa: 3.6
         },
         {
-          image: "s3.png",
-            name: "Moe",
+            image: 's3.png',
+            name: 'Moe',
             gpa: 3.2
         },
         {
-          image: "s4.png",
-            name: "Mary",
+            image: 's4.png',
+            name: 'Mary',
             gpa: 2.6
         }
-      ]
+    ]
 
-      const listMap = cardInfo.map(cardInfo => <Cards cardInfo = {cardInfo} />)
-      
-      return listMap
+    const cardMap = cardInfo.map(cards => <Students cards={cards} />)
+
+    return <div>
+        <h1>Students List</h1>
+        <div className='card-grid'>
+                    {cardMap}
+                    </div>
+    </div>
 }
 
 export default StudentList;
---
+++ src/components/Students.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import './studentlist.css'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+
+function Students({ cards }) {
+
+    return (
+        <Router>
+
+            <Card className='Students'>
+                <Card.Img variant='top' src={require(`../img/${cards.image}`)} />
+                <Card.Body>
+                    <Card.Title>{cards.name}</Card.Title>
+                    <Card.Text className='card-text'>
+                    <Routes>
+                        <Route path={`/grade/${cards.name}`} element={`${cards.gpa}`}></Route>
+                    </Routes>
+                    </Card.Text>
+                    <Link to={`/grade/${cards.name}`}>
+                        <Button>Show Grade</Button>
+                    </Link>
+                    </Card.Body>
+                    </Card>
+    </Router>
+    )
+}
+
+export default Students;
